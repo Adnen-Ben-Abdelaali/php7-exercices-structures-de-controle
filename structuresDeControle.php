5889532e4fb72a21ef110183c4cae8b078a59a90
AdnenBenAbdelaali <benabdelaaliadnen@gmail.com>
2020-01-27 19:19:18 +0100

    fichier php renommé

---
+++ structuresDeControle.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Rédigez une expression conditionnelle pour tester si un nombre est à la fois un multiple
+de 3 et de 5.
+*/
+
+$a = 8;
+if( ($a % 3 === 0) && ($a % 5 === 0) ) {
+  echo "Le nombre $a est divisible par n et par 6";  
+}else {
+  echo "le nombre $a n'est pas divisible par 3 et par 6 à la fois";
+}
+
+
+
+
+
+?>
